d990801bfa60fe644ff56390a683b46d2f76aba7
Arab_Theeb <theeb1230@gmail.com>
2026-09-04 17:05:43 +0300

    Restore production runtime src/entrypoint.js

--- src/entrypoint.js
+++ src/entrypoint.js
@@ -15,19 +15,17 @@ function start(env = process.env) {
   switch (role) {
     case RUNTIME_ROLES.ALL:
     case RUNTIME_ROLES.API:
-      return require(
-        "./server"
-      );
+      return require("./server").startServer();
 
     case RUNTIME_ROLES.REFRESH_WORKER:
       return require(
         "./workers/refresh-worker"
-      ).startRefreshWorker();
+      ).startRefreshWorker({ signal: env.THEEB_SIGNAL });
 
     case RUNTIME_ROLES.HEALTH_WORKER:
       return require(
         "./workers/health-worker"
-      ).startHealthWorker();
+      ).startHealthWorker({ signal: env.THEEB_SIGNAL });
 
     default:
       throw new Error(
@@ -37,8 +35,19 @@ function start(env = process.env) {
 }
 
 if (require.main === module) {
+  const controller = new AbortController();
+  const { installShutdownHandlers } = require("./runtime/shutdown");
+
   Promise.resolve()
-    .then(() => start())
+    .then(() => {
+      const runtime = start({ ...process.env, THEEB_SIGNAL: controller.signal });
+      installShutdownHandlers({
+        controller,
+        server: runtime && typeof runtime.close === "function" ? runtime : null,
+        completion: runtime && typeof runtime.then === "function" ? runtime : null
+      });
+      return runtime;
+    })
     .catch(error => {
     console.error(
       JSON.stringify({
