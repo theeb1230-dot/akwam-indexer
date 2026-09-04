051e6d4187645c8410bc43970373dfe2c2750499
Arab_Theeb <theeb1230@gmail.com>
2026-09-05 01:38:23 +0300

    Add shared Theeb Arab mobile brand contract

---
+++ clients/dart/lib/theeb_brand.dart
@@ -0,0 +1,15 @@
+class TheebBrand {
+  const TheebBrand._();
+
+  static const productNameAr = 'ذيب العرب';
+  static const productNameEn = 'Theeb Arab';
+  static const taglineAr = 'بحث موحد، مشاهدة وتحميل باختيارك';
+
+  static const backgroundHex = '#0b0b0b';
+  static const surfaceHex = '#141414';
+  static const surfaceAltHex = '#1b1b1b';
+  static const textHex = '#f5f5f5';
+  static const mutedHex = '#a8a8a8';
+  static const accentHex = '#d8c39a';
+  static const accentSoftHex = '#efe3c9';
+}
