7bc5a129ed280beabdcbb90db9fc3ebfde45bcd2
Arab_Theeb <theeb1230@gmail.com>
2026-09-05 03:19:19 +0300

    Add Android TV client target config

--- clients/dart/lib/theeb_client_config.dart
+++ clients/dart/lib/theeb_client_config.dart
@@ -1,7 +1,10 @@
+enum TheebClientTarget { mobile, tv }
+
 class TheebClientConfig {
   const TheebClientConfig({
     required this.baseUri,
     this.clientVersion = '0.1.0',
+    this.target = TheebClientTarget.mobile,
   });
 
   factory TheebClientConfig.fromEnvironment() {
@@ -9,9 +12,21 @@ class TheebClientConfig {
       'THEEB_API_BASE_URL',
       defaultValue: 'http://127.0.0.1:8080/',
     );
-    return TheebClientConfig(baseUri: Uri.parse(raw));
+    const targetRaw = String.fromEnvironment(
+      'THEEB_TARGET',
+      defaultValue: 'mobile',
+    );
+    return TheebClientConfig(
+      baseUri: Uri.parse(raw),
+      target: targetRaw == 'tv'
+          ? TheebClientTarget.tv
+          : TheebClientTarget.mobile,
+    );
   }
 
   final Uri baseUri;
   final String clientVersion;
+  final TheebClientTarget target;
+
+  bool get isTv => target == TheebClientTarget.tv;
 }
